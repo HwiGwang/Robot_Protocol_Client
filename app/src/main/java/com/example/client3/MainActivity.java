972c0f1d5c7bb35722c87efdc99636e22c4ae4c8
Hwi Gwang <hwi-gwang@naver.com>
2023-12-04 08:45:10 +0900

    2023 12 04

--- app/src/main/java/com/example/client3/MainActivity.java
+++ app/src/main/java/com/example/client3/MainActivity.java
@@ -36,115 +36,68 @@ public class MainActivity extends AppCompatActivity {
                 socket = new Socket("192.168.35.149", 14321);
 
                 // MakeType00~등을 이용해서 객체 생성 << 이건 서버로 데이터를 보내기위해 테스트한 것
-                Make sendData0 = new MakeType01((byte) 0x3, (byte) 0x01,0, 3.0f, 4.0f, 5.0f,
-                        6.0f,0,0,0,1);
+                Make sendData0 = new Make_User((byte) 0x4, (byte) 0x01, 0, 3.0f, 4.0f, 5.0f,
+                        6.0f, 0, 0, 0, 1);
 
-                Make sendData1 = new MakeType02((byte) 0x4, (byte) 0x02,(byte) 0x03,(byte)0x00, "Move", 3.0f, 4.0f, 5.0f,
-                        6.0f,0,0,0,1,2,3,4,5,6,7,8,9,10,11,
-                        12,13,14,15,16,17,18,19,20,21,(byte)0x00,(byte)0x01);
-                Make sendData2 = new MakeType04((byte) 0x05, (byte) 0x01, (byte) 0x00, (byte) 0x00, 1,2,3,4,5,6,7,8,9,10,
-                        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
+                Make sendData1 = new Make_Move((byte) 0x4, (byte) 0x01, (byte) 0x03, (byte) 0x00, "Move", 3.0f, 4.0f, 5.0f,
+                        6.0f, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+                        12, 13, 14, 15, 16, 17, 18, 19, 20, 21, (byte) 0x00, (byte) 0x01);
+                Make sendData2 = new Make_Circle((byte) 0x05, (byte) 0x01, (byte) 0x00, (byte) 0x00, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+                Make sendData3 = new Make_Debug((byte) 0x07, (byte) 0x00, "Debug 확인");
+                Make sendData4 = new Make_Set((byte)0x08, (byte)0x00, 0,0,0,0,0,0,0,0,0,0,0,(byte) 0x00,(byte) 0x00,
+                        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,(byte) 0x00);
+
+
                 ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-                objectOutputStream.writeObject(sendData2);
+                objectOutputStream.writeObject(sendData4);
 
                 // 서버로부터 데이터를 읽는 부분
                 InputStream in = socket.getInputStream();
                 ObjectInputStream objectInputStream = new ObjectInputStream(in);
                 Make receivedData = (Make) objectInputStream.readObject();
+
+                Log.d(TAG, "서버로부터 받은 데이터: " + receivedData);
+
                 objectInputStream.close();
 
-                // MakeType00 또는 MakeType01등으로 형변환
-                if (receivedData instanceof MakeType00) {
-                    MakeType00 receivedDataOfType00 = (MakeType00) receivedData;
-                    return "Received: MainCommandType=" + receivedDataOfType00.getMainCommandType() +
-                            ", SubCommandType=" + receivedDataOfType00.getSubCommandType() +
-                            ", X=" + receivedDataOfType00.getX() +
-                            ", Y=" + receivedDataOfType00.getY() +
-                            ", Z=" + receivedDataOfType00.getZ() +
-                            ", RX=" + receivedDataOfType00.getRX() +
-                            ", RY=" + receivedDataOfType00.getRY() +
-                            ", RZ=" + receivedDataOfType00.getRZ() +
-                            ", Dist=" + receivedDataOfType00.getDist() +
-                            ", Ori=" + receivedDataOfType00.getOri() +
-                            ", Joint=" + receivedDataOfType00.getJoint();
-                } else if (receivedData instanceof MakeType01) {
-                    MakeType01 receivedDataOfType01 = (MakeType01) receivedData;
-                    return "Received: MainCommandType=" + receivedDataOfType01.getMainCommandType() +
-                            ", SubCommandType=" + receivedDataOfType01.getSubCommandType() +
-                            ", Base=" + receivedDataOfType01.getBase() +
-                            ", Shoulder=" + receivedDataOfType01.getShoulder() +
-                            ", Elbow=" + receivedDataOfType01.getElbow() +
-                            ", Wrist1=" + receivedDataOfType01.getWrist1() +
-                            ", Wrist2=" + receivedDataOfType01.getWrist2() +
-                            ", Wrist3=" + receivedDataOfType01.getWrist3() +
-                            ", Dist=" + receivedDataOfType01.getDist() +
-                            ", Ori=" + receivedDataOfType01.getOri() +
-                            ", Joint=" + receivedDataOfType01.getJoint();
-                }else if (receivedData instanceof MakeType02) {
-                    MakeType02 receivedDataOfType02 = (MakeType02) receivedData;
-                    // MakeType02에 대한 처리를 여기에 추가
-                    return "Received: MainCommandType=" + receivedDataOfType02.getMainCommandType() +
-                            ", SubCommandType=" + receivedDataOfType02.getSubCommandType() +
-                            ", Type =" + receivedDataOfType02.getType() +
-                            ", MoveType=" + receivedDataOfType02.getMoveType() +
-                            ", Name=" + receivedDataOfType02.getName() +
-                            ", Speed=" + receivedDataOfType02.getSpeed() +
-                            ", Acc=" + receivedDataOfType02.getAcc() +
-                            ", FinishAt=" + receivedDataOfType02.getFinishAt() +
-                            ", StoppingTime=" + receivedDataOfType02.getStoppingTime() +
-                            ", X=" + receivedDataOfType02.getX() +
-                            ", Y=" + receivedDataOfType02.getY() +
-                            ", Z=" + receivedDataOfType02.getZ() +
-                            ", RX=" + receivedDataOfType02.getRX() +
-                            ", RY=" + receivedDataOfType02.getRY() +
-                            ", RZ=" + receivedDataOfType02.getRZ() +
-                            ", Base=" + receivedDataOfType02.getBase() +
-                            ", Shoulder=" + receivedDataOfType02.getShoulder() +
-                            ", Elbow=" + receivedDataOfType02.getElbow() +
-                            ", Wrist1=" + receivedDataOfType02.getWrist1() +
-                            ", Wrist2=" + receivedDataOfType02.getWrist2() +
-                            ", Wrist3=" + receivedDataOfType02.getWrist3() +
-                            ", J0=" + receivedDataOfType02.getJ0() +
-                            ", J1=" + receivedDataOfType02.getJ1() +
-                            ", J2=" + receivedDataOfType02.getJ2() +
-                            ", J3=" + receivedDataOfType02.getJ3() +
-                            ", J4=" + receivedDataOfType02.getJ4() +
-                            ", J5=" + receivedDataOfType02.getJ5() +
-                            ", JJ0=" + receivedDataOfType02.getJJ0() +
-                            ", JJ1=" + receivedDataOfType02.getJJ1() +
-                            ", JJ2=" + receivedDataOfType02.getJJ2() +
-                            ", JJ3=" + receivedDataOfType02.getJJ3() +
-                            ", JJ4=" + receivedDataOfType02.getJJ4() +
-                            ", JJ5=" + receivedDataOfType02.getJJ5() +
-                            ", Reference_Point=" + receivedDataOfType02.getReference_Point() +
-                            ", Reference_Coordiante=" + receivedDataOfType02.getReference_Coordinate();
-                }
-
-                else {
-                    // Handle other types if needed
-                    return "Received: Unknown Make type";
+                if (receivedData instanceof Make_Get) {
+                    Make_Get makeGetData = (Make_Get) receivedData;
+                    return "서버에서 받은 Make_Get 객체: " +
+                            "\nMainCommandType: " + makeGetData.getMainCommandType() +
+                            "\nSubCommandType: " + makeGetData.getSubCommandType() +
+                            "\nx: " + makeGetData.getX() +
+                            "\ny: " + makeGetData.getY() +
+                            "\nz: " + makeGetData.getZ() +
+                            "\nrx: " + makeGetData.getRx() +
+                            "\nry: " + makeGetData.getRy() +
+                            "\nrz: " + makeGetData.getRz() +
+                            "\nBase: " + makeGetData.getBase()+
+                            "\nShoulder: " + makeGetData.getShoulder() +
+                            "\nElbow: " + makeGetData.getElbow() +
+                            "\nWrist1: " + makeGetData.getWrist1() +
+                            "\nWrist2: " + makeGetData.getWrist2() +
+                            "\nWrist3: " + makeGetData.getWrist3();
+                } else if (receivedData instanceof Make_Debug) {
+                    Make_Debug makeDebugData = (Make_Debug) receivedData;
+                    return "서버에서 받은 Make_Debug 객체: " +
+                            "\nMainCommandType: " + makeDebugData.getMainCommandType() +
+                            "\nSubCommandType: " + makeDebugData.getSubCommandType() +
+                            "\nName: " + makeDebugData.getName();
+                } else {
+                    return "서버에서 받은 데이터: " + receivedData.toString();
                 }
 
             } catch (IOException | ClassNotFoundException e) {
                 Log.e(TAG, "Error: " + e.getMessage());
-                return "오류 발생";
+                return "오류 발생: " + e.getMessage(); // 오류 메시지를 반환
             }
         }
 
         @Override
         protected void onPostExecute(String result) {
-            if (socket != null && !socket.isClosed()) {
-                try {
-                    socket.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            Log.d(TAG, "서버로부터 받은 메시지: " + result);
-
-            // 결과를 UI에 표시
-            receivedMessageTextView.setText("서버로부터 받은 메시지: " + result);
+            // UI 업데이트
+            receivedMessageTextView.setText(result);
         }
     }
 }
--- app/src/main/java/com/example/client3/Make.java
+++ app/src/main/java/com/example/client3/Make.java
@@ -18,7 +18,7 @@ public abstract class Make implements Serializable {
     public abstract byte getSubCommandType();
 }
 
-class MakeType00 extends Make { //MakeType00은 maincommandtype이 0x00~0x02까지
+class Make_Jog extends Make { //MakeType00은 maincommandtype이 0x00~0x02까지
     private byte subCommandType;
     private float x;
     private float y;
@@ -30,7 +30,7 @@ class MakeType00 extends Make { //MakeType00은 maincommandtype이 0x00~0x02까�
     private float ori;
     private float joint;
 
-    public MakeType00(byte mainCommandType, byte subCommandType, float x, float y, float z, float rx, float ry, float rz,float dist, float ori, float joint) {
+    public Make_Jog(byte mainCommandType, byte subCommandType, float x, float y, float z, float rx, float ry, float rz,float dist, float ori, float joint) {
         super(mainCommandType);
         this.subCommandType = subCommandType;
         this.x = x;
@@ -88,7 +88,7 @@ class MakeType00 extends Make { //MakeType00은 maincommandtype이 0x00~0x02까�
 }
 
 
-class MakeType01 extends Make { //maincommandtype이 0x03(User)일때
+class Make_User extends Make { //maincommandtype이 0x03(User)일때
     private byte subCommandType;
     private float base;
     private float shoulder;
@@ -100,7 +100,7 @@ class MakeType01 extends Make { //maincommandtype이 0x03(User)일때
     private float ori;
     private float joint;
 
-    public MakeType01(byte mainCommandType, byte subCommandType, float base, float shoulder, float elbow, float wrist1, float wrist2, float wrist3, float dist, float ori, float joint) {
+    public Make_User(byte mainCommandType, byte subCommandType, float base, float shoulder, float elbow, float wrist1, float wrist2, float wrist3, float dist, float ori, float joint) {
         super(mainCommandType);
         this.subCommandType = subCommandType;
         this.base = base;
@@ -155,7 +155,9 @@ class MakeType01 extends Make { //maincommandtype이 0x03(User)일때
     }
 }
 
-class MakeType02 extends Make { //maincommand type이 0x04(Move)
+
+
+class Make_Move extends Make { //maincommand type이 0x04(Move)
     private byte subCommandType;
     private byte type;
     private byte moveType;
@@ -191,9 +193,9 @@ class MakeType02 extends Make { //maincommand type이 0x04(Move)
     private byte reference_Point;
     private byte reference_Coordinate;
 
-    public MakeType02(byte mainCommandType, byte subCommandType, byte type, byte moveType, String name, float speed, float acc, float finish_at, float stopping_time, float x,
-                      float y, float z, float rx, float ry, float rz, float base, float shoulder, float elbow, float wrist1, float wrist2, float wrist3,float j0,
-                      float j1, float j2, float j3, float j4, float j5, float jj0,float jj1,float jj2,float jj3,float jj4,float jj5, byte reference_Point, byte reference_Coordinate) {
+    public Make_Move(byte mainCommandType, byte subCommandType, byte type, byte moveType, String name, float speed, float acc, float finish_at, float stopping_time, float x,
+                     float y, float z, float rx, float ry, float rz, float base, float shoulder, float elbow, float wrist1, float wrist2, float wrist3,float j0,
+                     float j1, float j2, float j3, float j4, float j5, float jj0,float jj1,float jj2,float jj3,float jj4,float jj5, byte reference_Point, byte reference_Coordinate) {
         super(mainCommandType);
         this.subCommandType = subCommandType;
         this.type = type;
@@ -413,7 +415,7 @@ class MakeType02 extends Make { //maincommand type이 0x04(Move)
         return reference_Coordinate;
     }
 }
-class MakeType04 extends Make { // maincommand type이 0x06(Circle)
+class Make_Circle extends Make { // maincommand type이 0x06(Circle)
     private byte subCommandType;
     private byte ori_Type;
     private byte type;
@@ -447,9 +449,9 @@ class MakeType04 extends Make { // maincommand type이 0x06(Circle)
     private float wrist3_2;
     private float degree;
 
-    public MakeType04(byte mainCommandType, byte subCommandType, byte ori_Type, byte type, float speed, float acc, float finish_at, float stopping_time, float x1,
-                      float y1, float z1, float rx1, float ry1, float rz1, float base1, float shoulder1, float elbow1, float wrist1_1, float wrist2_1, float wrist3_1,
-                      float x2, float y2, float z2, float rx2, float ry2, float rz2, float base2, float shoulder2, float elbow2, float wrist1_2, float wrist2_2, float wrist3_2, float degree) {
+    public Make_Circle(byte mainCommandType, byte subCommandType, byte ori_Type, byte type, float speed, float acc, float finish_at, float stopping_time, float x1,
+                       float y1, float z1, float rx1, float ry1, float rz1, float base1, float shoulder1, float elbow1, float wrist1_1, float wrist2_1, float wrist3_1,
+                       float x2, float y2, float z2, float rx2, float ry2, float rz2, float base2, float shoulder2, float elbow2, float wrist1_2, float wrist2_2, float wrist3_2, float degree) {
         super(mainCommandType);
         this.subCommandType = subCommandType;
         this.ori_Type = ori_Type;
@@ -619,10 +621,338 @@ class MakeType04 extends Make { // maincommand type이 0x06(Circle)
     public float getDegree() {
         return degree;
     }
+
+
+}
+class Make_Get extends Make { //maincommandtype이 0x03(User)일때
+    private float x;
+    private float y;
+    private float z;
+    private float rx;
+    private float ry;
+    private float rz;
+    private float base;
+    private float shoulder;
+    private float elbow;
+    private float wrist1;
+    private float wrist2;
+    private float wrist3;
+
+
+    public Make_Get(byte mainCommandType, float x, float y, float z, float rx, float ry, float rz, float base, float shoulder, float elbow, float wrist1, float wrist2, float wrist3) {
+        super(mainCommandType);
+        this.x=x;
+        this.y=y;
+        this.z=z;
+        this.rx=rx;
+        this.ry=ry;
+        this.rz=rz;
+        this.base = base;
+        this.shoulder = shoulder;
+        this.elbow = elbow;
+        this.wrist1 = wrist1;
+        this.wrist2 = wrist2;
+        this.wrist3 = wrist3;
+
+
+    }
+    public float getX() {
+        return x;
+    }
+    public float getY() {
+        return y;
+    }
+    public float getZ() {
+        return z;
+    }
+    public float getRx() {
+        return rx;
+    }
+    public float getRy() {
+        return ry;
+    }
+    public float getRz() {
+        return rz;
+    }
+
+    public float getBase() {
+        return base;
+    }
+
+    public float getShoulder() {
+        return shoulder;
+    }
+
+    public float getElbow() {
+        return elbow;
+    }
+
+    public float getWrist1() {
+        return wrist1;
+    }
+
+    public float getWrist2() {
+        return wrist2;
+    }
+
+    public float getWrist3() {
+        return wrist3;
+    }
+
+
+    @Override
+    public byte getSubCommandType() {
+        return 0;
+    }
 }
 
+class Make_Debug extends Make {
+    private byte subCommandType;
+    private String name;
 
+    public Make_Debug(byte mainCommandType, byte subCommandType, String name) {
+        super(mainCommandType);
+        if (subCommandType == 0x00) { // From Control Box
+            this.name = name;}
+        if(subCommandType == 0x01) {
+            this.name = name;
+        }
 
+    }
+
+    public byte getSubCommandType() {
+        return subCommandType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
+
+class Make_Set extends Make {
+    private byte subCommandType;
+    private float threshold;
+    private float mass;
+    private float mass_x;
+    private float mass_y;
+    private float mass_z;
+    private float base_x;
+    private float base_y;
+    private float base_z;
+    private float base_rx;
+    private float base_ry;
+    private float base_rz;
+    private byte inbox_Box;
+    private byte inbox_Mode;
+    private float tcp_x;
+    private float tcp_y;
+    private float tcp_z;
+    private float tcp_rx;
+    private float tcp_ry;
+    private float tcp_rz;
+    private float box_x_width;
+    private float box_y_width;
+    private float box_z_width;
+    private float box_x;
+    private float box_y;
+    private float box_z;
+    private float workspace_x_width;
+    private float workspace_y_width;
+    private float workspace_z_width;
+    private float workspace_x;
+    private float workspace_y;
+    private float workspace_z;
+    private byte enable_Workspace;
+
+    public Make_Set(byte mainCommandType, byte subCommandType, float threshold, float mass, float mass_x, float mass_y, float mass_z,
+                    float base_x, float base_y, float base_z, float base_rx, float base_ry, float base_rz, byte inbox_Box, byte inbox_Mode,
+                    float tcp_x, float tcp_y, float tcp_z, float tcp_rx, float tcp_ry, float tcp_rz, float box_x_width, float box_y_width,
+                    float box_z_width, float box_x, float box_y, float box_z, float workspace_x_width, float workspace_y_width, float workspace_z_width,
+                    float workspace_x, float workspace_y, float workspace_z, byte enable_Workspace) {
+        super(mainCommandType);
+        this.subCommandType = subCommandType;
+
+        if (subCommandType == 0x00) { // 충돌감지감도조절
+            this.threshold = threshold;
+        }
+        if (subCommandType == 0x01) { // TCP하중
+            this.mass = mass;
+            this.mass_x = mass_x;
+            this.mass_y = mass_y;
+            this.mass_z = mass_z;
+        }
+        if (subCommandType == 0x02) { // 동작오프셋
+            this.base_x = base_x;
+            this.base_y = base_y;
+            this.base_z = base_z;
+            this.base_rx = base_rx;
+            this.base_ry = base_ry;
+            this.base_rz = base_rz;
+        }
+        if (subCommandType == 0x03) { // 인박스
+            this.inbox_Box = inbox_Box;
+            this.inbox_Mode = inbox_Mode;
+        }
+        if (subCommandType == 0x04) { // TCP 좌표 임시 설정
+            this.tcp_x = tcp_x;
+            this.tcp_y = tcp_y;
+            this.tcp_z = tcp_z;
+            this.tcp_rx = tcp_rx;
+            this.tcp_ry = tcp_ry;
+            this.tcp_rz = tcp_rz;
+        }
+        if (subCommandType == 0x05) { // 툴 안전영역 설정
+            this.box_x_width = box_x_width;
+            this.box_y_width = box_y_width;
+            this.box_z_width = box_z_width;
+            this.box_x = box_x;
+            this.box_y = box_y;
+            this.box_z = box_z;
+        }
+        if (subCommandType == 0x06) { //작업안전영역설정
+            this.workspace_x_width = workspace_x_width;
+            this.workspace_y_width = workspace_y_width;
+            this.workspace_z_width = workspace_z_width;
+            this.workspace_x = workspace_x;
+            this.workspace_y = workspace_y;
+            this.workspace_z = workspace_z;
+            this.enable_Workspace = enable_Workspace;
+        }
+    }
+
+    public byte getSubCommandType() {
+        return subCommandType;
+    }
+
+    public float getThreshold() {
+        return threshold;
+    }
+
+    public float getMass() {
+        return mass;
+    }
+
+    public float getMassX() {
+        return mass_x;
+    }
+
+    public float getMassY() {
+        return mass_y;
+    }
+
+    public float getMassZ() {
+        return mass_z;
+    }
+
+    public float getBaseX() {
+        return base_x;
+    }
+
+    public float getBaseY() {
+        return base_y;
+    }
+
+    public float getBaseZ() {
+        return base_z;
+    }
+
+    public float getBaseRX() {
+        return base_rx;
+    }
+
+    public float getBaseRY() {
+        return base_ry;
+    }
+
+    public float getBaseRZ() {
+        return base_rz;
+    }
+
+    public byte getInboxBox() {
+        return inbox_Box;
+    }
+
+    public byte getInboxMode() {
+        return inbox_Mode;
+    }
+
+    public float getTCPX() {
+        return tcp_x;
+    }
+
+    public float getTCPY() {
+        return tcp_y;
+    }
+
+    public float getTCPZ() {
+        return tcp_z;
+    }
+
+    public float getTCPRX() {
+        return tcp_rx;
+    }
+
+    public float getTCPRY() {
+        return tcp_ry;
+    }
+
+    public float getTCPRZ() {
+        return tcp_rz;
+    }
+
+    public float getBoxXWidth() {
+        return box_x_width;
+    }
+
+    public float getBoxYWidth() {
+        return box_y_width;
+    }
+
+    public float getBoxZWidth() {
+        return box_z_width;
+    }
+
+    public float getBoxX() {
+        return box_x;
+    }
+
+    public float getBoxY() {
+        return box_y;
+    }
+
+    public float getBoxZ() {
+        return box_z;
+    }
+
+    public float getWorkspaceXWidth() {
+        return workspace_x_width;
+    }
+
+    public float getWorkspaceYWidth() {
+        return workspace_y_width;
+    }
+
+    public float getWorkspaceZWidth() {
+        return workspace_z_width;
+    }
+
+    public float getWorkspaceX() {
+        return workspace_x;
+    }
+
+    public float getWorkspaceY() {
+        return workspace_y;
+    }
+
+    public float getWorkspaceZ() {
+        return workspace_z;
+    }
+
+    public byte getEnableWorkspace() {
+        return enable_Workspace;
+    }
+}
 
 
 
